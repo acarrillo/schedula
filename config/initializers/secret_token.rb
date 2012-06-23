81ff6bde9759500f0844e257a29bc7dc86567f0e
Blake Elias <blakeelias@gmail.com>
2012-06-22 22:24:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoboApp::Application.config.secret_token = 'ad4328f5e9b49432990128987b496df5778dc5573cc61c0d077408c056f70ed61c1500363046b802f4277c5657ddd55e95f0c96e17cddcb86ceecc975889e04e'
