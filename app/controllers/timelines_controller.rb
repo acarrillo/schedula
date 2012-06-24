58b939ebe6e015926b858d3a3e963a75e5ed8800
Blake Elias <blakeelias@gmail.com>
2012-06-24 06:18:41 -0400

    Add JSON view for timeline

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -4,4 +4,17 @@ class TimelinesController < ApplicationController
 
   auto_actions :all
   
+  show_action :json
+  def json
+    @get = params[:id]
+    @timeline = Timeline.find(params[:id])
+    @name = @timeline.name
+    @events = @timeline.events
+    @s = ''
+    for @item in @events
+       @s += "{'id' : " + @item.id.to_s + ", 'name' : '" + @item.name + "', 'description' : '" + @item.description + "'}, "
+    end
+    return @s
+  end
+  
 end
--- config/hobo_routes.rb
+++ config/hobo_routes.rb
@@ -5,18 +5,6 @@
 HoboApp::Application.routes.draw do
 
 
-  # Resource routes for controller "events"
-  get 'events/new(.:format)', :as => 'new_event'
-  get 'events/:id/edit(.:format)' => 'events#edit', :as => 'edit_event'
-  get 'events/:id(.:format)' => 'events#show', :as => 'event', :constraints => { :id => %r([^/.?]+) }
-  post 'events(.:format)' => 'events#create', :as => 'create_event'
-  put 'events/:id(.:format)' => 'events#update', :as => 'update_event', :constraints => { :id => %r([^/.?]+) }
-  delete 'events/:id(.:format)' => 'events#destroy', :as => 'destroy_event', :constraints => { :id => %r([^/.?]+) }
-
-  # Show action routes for controller "events"
-  get 'events/:id/json(.:format)' => 'events#json', :as => 'event_json'
-
-
   # Resource routes for controller "timelines"
   get 'timelines(.:format)' => 'timelines#index', :as => 'timelines'
   get 'timelines/new(.:format)', :as => 'new_timeline'
@@ -26,11 +14,8 @@ HoboApp::Application.routes.draw do
   put 'timelines/:id(.:format)' => 'timelines#update', :as => 'update_timeline', :constraints => { :id => %r([^/.?]+) }
   delete 'timelines/:id(.:format)' => 'timelines#destroy', :as => 'destroy_timeline', :constraints => { :id => %r([^/.?]+) }
 
-
-  # Resource routes for controller "timeline_followships"
-  post 'timeline_followships(.:format)' => 'timeline_followships#create', :as => 'create_timeline_followship'
-  put 'timeline_followships/:id(.:format)' => 'timeline_followships#update', :as => 'update_timeline_followship', :constraints => { :id => %r([^/.?]+) }
-  delete 'timeline_followships/:id(.:format)' => 'timeline_followships#destroy', :as => 'destroy_timeline_followship', :constraints => { :id => %r([^/.?]+) }
+  # Show action routes for controller "timelines"
+  get 'timelines/:id/json(.:format)' => 'timelines#json', :as => 'timeline_json'
 
 
   # Index action routes for controller "users"
@@ -58,6 +43,24 @@ HoboApp::Application.routes.draw do
   match 'login(.:format)' => 'users#login', :as => 'user_login'
   get 'logout(.:format)' => 'users#logout', :as => 'user_logout'
   match 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
+
+
+  # Resource routes for controller "timeline_followships"
+  post 'timeline_followships(.:format)' => 'timeline_followships#create', :as => 'create_timeline_followship'
+  put 'timeline_followships/:id(.:format)' => 'timeline_followships#update', :as => 'update_timeline_followship', :constraints => { :id => %r([^/.?]+) }
+  delete 'timeline_followships/:id(.:format)' => 'timeline_followships#destroy', :as => 'destroy_timeline_followship', :constraints => { :id => %r([^/.?]+) }
+
+
+  # Resource routes for controller "events"
+  get 'events/new(.:format)', :as => 'new_event'
+  get 'events/:id/edit(.:format)' => 'events#edit', :as => 'edit_event'
+  get 'events/:id(.:format)' => 'events#show', :as => 'event', :constraints => { :id => %r([^/.?]+) }
+  post 'events(.:format)' => 'events#create', :as => 'create_event'
+  put 'events/:id(.:format)' => 'events#update', :as => 'update_event', :constraints => { :id => %r([^/.?]+) }
+  delete 'events/:id(.:format)' => 'events#destroy', :as => 'destroy_event', :constraints => { :id => %r([^/.?]+) }
+
+  # Show action routes for controller "events"
+  get 'events/:id/json(.:format)' => 'events#json', :as => 'event_json'
 
   namespace :admin do
 
